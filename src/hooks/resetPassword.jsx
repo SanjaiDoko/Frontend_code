6b61dbc84db0f90474830034092c5ed12e892abf
Balaji <rbalaji111299@gmail.com>
2023-08-30 11:04:35 +0530

    fixed cross error

--- src/hooks/resetPassword.jsx
+++ src/hooks/resetPassword.jsx
@@ -2,8 +2,10 @@ import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import { fetchData } from "../helper";
 import { URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 const useResetpasswordData = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: (data) => {
       let payload = data;
@@ -12,7 +14,7 @@ const useResetpasswordData = () => {
         {
           url: URL + "user/forgotpassword",
           method: "POST",
-          isAuthRequired: true,
+          //   isAuthRequired: true,
           isEncryptedPayload: false,
         },
         { data: [payload] }
@@ -20,6 +22,7 @@ const useResetpasswordData = () => {
     },
     onSuccess: () => {
       toast.success("Check your Inbox");
+      navigate("/login");
     },
     onError: (error) => {
       toast.error(error.message.split(":")[1]);
